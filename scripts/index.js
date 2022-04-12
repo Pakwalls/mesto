81f8b831066532e08ac2e4e2b44af5035498eda4
Vyacheslav Petish <vyacheslav.petish@yandex.ru>
2022-04-12 21:53:48 +0300

    fix: last fixes of reviewer

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,4 @@
-const initialCards = [
-  { name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'},
-  { name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'},
-  { name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'},
-  { name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'},
-  { name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'},
-  { name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'}
-]; 
+import {initialCards} from './cards.js';
 
 const profileCard = document.querySelector(`.profile`);
 const profileEditBtn = profileCard.querySelector(`.profile__edit-btn`);
@@ -68,15 +55,13 @@ function getElement(item) {
   return cardClone;
 }
 
-
-
 formAdd.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const newCard = {
     name: placeInput.value,
     link: placeLinkInput.value,
   };
-  card = getElement(newCard);
+  const card = getElement(newCard);
   list.prepend(card);
   
   closePopupWindow(popupAddWindow);
