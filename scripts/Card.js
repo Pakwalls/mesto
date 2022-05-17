633aa108c7863417a722277e553435d362b34965
Vyacheslav Petish <vyacheslav.petish@yandex.ru>
2022-05-17 14:23:45 +0300

    added class card

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,17 +1,40 @@
 class Card {
   _title;
   _image;
-  _cardTemplate;
+  _template;
+  _cardExample;
 
 
-  constructor(title, image, cardTemplate) {
+  constructor(title, image, template) {
     this._title = title;
     this._image = image;
-    this._cardTemplate = cardTemplate;
+    this._template = template;
   }
 
-  _getTemplate() {
+  //функция колбэк для удаления карточки
+  _delCardClickHandler() {
 
   }
 
+  //функция колбэк для лайка карточки
+  _handleFeedback() {
+
+  }
+
+  _getCard() {
+    this._cardExample = this._template.cloneNode(true).querySelector(`.article`);
+    this._cardExample.querySelector(`.article__title`).textContent = this._title;
+    this._cardExample.querySelector(`.article__img`).src = this._image;
+    this._cardExample.querySelector(`.article__img`).alt = `Фотография ${this._title}`;
+
+    this._cardExample.querySelector(`.article__del-btn`).addEventListener('click' () => {
+      //функция колбэк для удаления карточки
+    })
+
+    this._cardExample.querySelector(`.article__feedback`).addEventListener('click' () => {
+      //функция колбэк для лайка карточки
+    })
+
+    return this._cardExample;
+  }
 }
