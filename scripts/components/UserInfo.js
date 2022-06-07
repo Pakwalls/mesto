62994211c09b2f38c3166b15ba4833ad84fb0751
Vyacheslav Petish <vyacheslav.petish@yandex.ru>
2022-06-07 15:20:08 +0300

    cleaning out code

--- scripts/components/UserInfo.js
+++ scripts/components/UserInfo.js
@@ -1,4 +1,4 @@
-export class UserInfo {
+export default class UserInfo {
   constructor({ userNameSelector, userJobSelector }) {
     this._userName = document.querySelector(userNameSelector);
     this._userJob = document.querySelector(userJobSelector);
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,23 +1,10 @@
 import { initialCards } from '../utils/initCards.js';
-import { openPopupWindow, closePopupWindow } from '../utils/utils.js';
 import { Validator } from '../components/FormValidator.js';
 import {
   profileEditBtn,
   profileAddbtn,
-  popupEditWindow,
-  formEdit,
-  editFormClosebtn,
   nameInput,
   jobInput,
-  popupAddWindow,
-  formAdd,
-  addFormCloseBtn,
-  placeInput,
-  placeLinkInput,
-  popupZoom,
-  popupZoomCloseBtn,
-  nameInProfile,
-  jobInProfile,
   cardElementsList,
   configData,
 } from '../utils/data.js';
@@ -26,7 +13,7 @@ import Card from '../components/Card.js';
 import Section from '../components/Section.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import PopupWithImage from '../components/PopupWithImage.js';
-import { UserInfo } from '../components/UserInfo.js';
+import UserInfo from '../components/UserInfo.js';
 
 // ---------------------------------------------------------------------------------- собрать карты в контейнер секции
 const cardList = new Section({ 
@@ -79,55 +66,6 @@ profileEditBtn.addEventListener('click', () => {
 
   profileForm.open();
 })
-
-
-
-
-// ---------------------------------------------------------------------------------- 
-// function handleFormAddSubmit(evt) {
-//   evt.preventDefault();
-//   const validate = new Validator(configData, formAdd)
-
-//   const newCard = {
-//     name: placeInput.value,
-//     link: placeLinkInput.value,
-//   };
-//   const card = new Card(newCard, `.template`);
-  
-//   cardElementsList.prepend(card.showElement());
-//   closePopupWindow(popupAddWindow);
-  
-//   formAdd.reset();
-//   validate.toggleButtonCondition(); // подтянут из FormValidator.js метод
-// };
-
-// function handleProfileFormSubmit (evt) {
-//   evt.preventDefault();
-//   nameInProfile.textContent = nameInput.value;
-//   jobInProfile.textContent = jobInput.value;
-  
-//   closePopupWindow(popupEditWindow);
-// };
-
-// function openEditWindow() {
-//   nameInput.value = nameInProfile.textContent;
-//   jobInput.value = jobInProfile.textContent;
-
-//   openPopupWindow(popupEditWindow);
-// };
-
-// ---------------------------------------------------------------------------------- слушатели
-// formAdd.addEventListener('submit', handleFormAddSubmit);
-// formEdit.addEventListener('submit', handleProfileFormSubmit);
-
-// profileEditBtn.addEventListener('click', openEditWindow);
-// editFormClosebtn.addEventListener('click', () => closePopupWindow(popupEditWindow));
-
-// profileAddbtn.addEventListener('click', () => openPopupWindow(popupAddWindow));
-// addFormCloseBtn.addEventListener('click', () => closePopupWindow(popupAddWindow));
-
-// popupZoomCloseBtn.addEventListener('click', () => closePopupWindow(popupZoom));
-
 
 // ---------------------------------------------------------------------------------- включение валидации
 const forms = document.querySelectorAll(configData.formSelector)
